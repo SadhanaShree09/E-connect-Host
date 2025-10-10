86708e79ded43708d3884250cb1c07f3ff7451f9
harrines <harrines0920@gmail.com>
2025-10-11 00:06:13 +0530

    adminchat access change

--- backend/Server.py
+++ backend/Server.py
@@ -235,7 +235,10 @@ if allowed_origins_env:
     origins = [origin.strip() for origin in allowed_origins_env.split(",")]
 else:
     origins = [
-       "*"
+        "https://e-connect-host-frontend.vercel.app",
+        "https://econnect-frontend-wheat.vercel.app",
+        "http://localhost:5173",
+        "http://localhost:5174"
     ]
 
 print(f"CORS Allowed Origins: {origins}")
